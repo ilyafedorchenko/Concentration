04a164596448ad2197dcaa91608b0a9acbd16365
Ilya Fedorchenko <ilya.fedorchenko@gmail.com>
2019-01-06 04:54:04 +0300

    D2
    Day 2 (lectures)

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  
+  lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+  
+  var flipCount = 0 {
+    didSet {
+      flipCountLabel.text = "Flips: \(flipCount)"
+    }
   }
-
-
+  @IBOutlet var cardButtons: [UIButton]!
+  
+  @IBOutlet weak var flipCountLabel: UILabel!
+  
+  @IBAction func touchCard(_ sender: UIButton) {
+    flipCount += 1
+    if let cardNumber = cardButtons.firstIndex(of: sender) {
+      game.chooseCard(at: cardNumber)
+      updateViewFromModel()
+    } else {
+      print("not in cardButtons")
+    }
+    
+  }
+  
+  func updateViewFromModel() {
+    for index in cardButtons.indices {
+      
+      let button = cardButtons[index]
+      let card = game.cards[index]
+      
+      if card.isFaceUp {
+        button.setTitle(emoji(for: card), for: UIControl.State.normal)
+        button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+      } else {
+        button.setTitle("", for: UIControl.State.normal)
+        button.backgroundColor = card.isMatched ? UIColor.clear : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+      }
+    }
+  }
+  
+  var emojiChoices = ["🦇","😱","🎃","👻", "🙀", "😈", "🍭", "🍬", "🍎"]
+  var emoji = [Int:String]()
+  
+  func emoji (for card: Card) -> String {
+    if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+      let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+      emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+    }
+    return emoji[card.identifier] ?? "?"
+  }
+  
 }
 
